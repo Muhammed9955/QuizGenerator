30a079f90fdaa49d2ec919ca99d9e7e8b26489c6
Muhammed khaled <muhammmed.k95@gmail.com>
2020-11-12 14:03:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,223 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+import {
+  Button,
+  Checkbox,
+  Segment,
+  Form,
+  Dropdown,
+  Icon,
+} from "semantic-ui-react";
+import { useState } from "react";
+import { football, Geography } from "./questionsStore";
 
 function App() {
+  const queasionsTypesIntialState = {
+    easyQuestionsCount: 0,
+    mediumQuestionsCount: 0,
+    hardQuestionsCount: 0,
+  };
+  const [state, setState] = useState({});
+  const [questionsStore, setQuestionsStore] = useState([]);
+  const [showQuestions, setShowQuestions] = useState(false);
+  const [modifiedQuestion, setModifiedQuestion] = useState({});
+  const [showEditForm, setShowEditForm] = useState(false);
+  const [questionIndex, setQuestionIndex] = useState("");
+  // const [easyMark, setEasyMark] = useState(20)
+  // const [mediumMark, setMediumMark] = useState(50)
+  // const [hardMark, setHardMark] = useState(30)
+  const [queasionsTypes, setQueasionsTypes] = useState(
+    queasionsTypesIntialState
+  );
+  const copyFootball1 = [...football];
+  const copyFootball2 = [...football];
+  const copyGeography1 = [...Geography];
+  const copyGeography2 = [...Geography];
+  const compinedQuizFootball = [
+    ...copyFootball1.splice(0, 4),
+    ...copyGeography1.splice(4, 10),
+  ];
+  const compinedQuizGeography = [
+    ...copyGeography2.splice(0, 4),
+    ...copyFootball2.splice(4, 10),
+  ];
+  // console.log({football})
+  // console.log({Geography})
+
+  const handleChange = (e, { value }) => {
+    setState({ value });
+    console.log({ value });
+  };
+  const handleChangeQuestion = (e) => {
+    setModifiedQuestion({ text: e.target.value, questionIndex });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newquestionsStore = questionsStore.map((q, indx) =>
+      indx == questionIndex ? { ...q, text: modifiedQuestion.text } : q
+    );
+    setQuestionsStore([...newquestionsStore]);
+    setShowEditForm(false);
+  };
+
+  const showQuestionsFunc = () => {
+    if (!state?.value) {
+      alert("please select a topic first");
+    } else {
+      setShowQuestions(true);
+    }
+  };
+  const restQuestions = () => {
+    setShowQuestions(false);
+  };
+  const getQuestionsStore = () => {
+    if (state.value === "Geography") {
+      setQuestionsStore([...Geography]);
+    } else if (state.value === "football") {
+      setQuestionsStore([...football]);
+    } else if (state.value === "compinedFootball") {
+      setQuestionsStore([...compinedQuizFootball]);
+    } else if (state.value === "compinedGeography") {
+      setQuestionsStore([...compinedQuizGeography]);
+    }
+  };
+  const deleteQuestion = (index) => {
+    console.log({ index });
+    const newquestionsStore = questionsStore.filter((q, indx) => indx != index);
+    console.log({ newquestionsStore });
+    setQuestionsStore([...newquestionsStore]);
+  };
+  const editQuetion = (index) => {
+    const newquestionsStore = questionsStore.map((q, indx) => indx != index);
+    setQuestionsStore([...newquestionsStore]);
+  };
+
+  // const getQuestionsTypes = () => {
+  //   questionsStore.map(q =>{
+  //     // console.log({q})
+  //     // console.log("difficulty",q.difficulty)
+  //     if( q.difficulty == "easy") {
+  //     setQueasionsTypes({...queasionsTypes,easyQuestionsCount: queasionsTypes.easyQuestionsCount + 1})
+  //     } else if (q.difficulty == "medium") {
+  //     setQueasionsTypes({...queasionsTypes,mediumQuestionsCount: queasionsTypes.mediumQuestionsCount + 1})
+  //     } else {
+  //     setQueasionsTypes({...queasionsTypes,hardQuestionsCount: queasionsTypes.hardQuestionsCount + 1})
+  //     }
+  //   })
+  // }
+  // getQuestionsTypes()
+  // console.log({queasionsTypes})
+  // console.log({showQuestions})
+  // console.log({state})
+  // console.log({questionsStore})
+  console.log({ modifiedQuestion });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Segment className="app_header">
+        <Form>
+          <h3>Quiz</h3>
+          <Form.Field>Selected quiz criteria:</Form.Field>
+          <Form.Field>
+            <Checkbox
+              label="football topic"
+              name="football"
+              value="football"
+              onChange={handleChange}
+              checked={state.value === "football"}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox
+              label="Geography topic"
+              name="Geography"
+              value="Geography"
+              onChange={handleChange}
+              checked={state.value === "Geography"}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox
+              label="compined quiz , 60% football & 40% geography"
+              name="compinedFootball"
+              value="compinedFootball"
+              onChange={handleChange}
+              checked={state.value === "compinedFootball"}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox
+              label="compined quiz , 40% football & 60% geography"
+              name="compinedGeography"
+              value="compinedGeography"
+              onChange={handleChange}
+              checked={state.value === "compinedGeography"}
+            />
+          </Form.Field>
+        </Form>
+      </Segment>
+      <Segment raised>
+        {!showQuestions && (
+          <h4 style={{ color: "red" }}>
+            select quiz criteria from above then click on Generate Question
+            Paper to make new quiz{" "}
+          </h4>
+        )}
+        {showEditForm && (
+          <Form>
+            <Form.Field>
+              <input
+                placeholder={modifiedQuestion.text}
+                value={modifiedQuestion.text}
+                // value={modifiedQuestion}
+                onChange={(e) => handleChangeQuestion(e)}
+              />
+            </Form.Field>
+            <Button
+              type="submit"
+              onClick={(e) => {
+                handleSubmit(e);
+              }}
+            >
+              Submit
+            </Button>
+          </Form>
+        )}
+        {showQuestions &&
+          questionsStore.length > 0 &&
+          questionsStore.map((q, index) => (
+            <h4>
+              - {q.text}
+              (marks:{q.marks}).{" "}
+              <Icon
+                color="green"
+                name="edit"
+                onClick={() => {
+                  setShowEditForm(true);
+                  setQuestionIndex(index);
+                  setModifiedQuestion({ ...modifiedQuestion, text: q.text });
+                }}
+              />
+              <Icon
+                color="red"
+                name="delete"
+                onClick={() => deleteQuestion(index)}
+              />
+            </h4>
+          ))}
+      </Segment>
+
+      <Button
+        primary
+        onClick={() => {
+          showQuestionsFunc();
+          getQuestionsStore();
+        }}
+      >
+        Generate Questions Paper
+      </Button>
+      <Button color="black" onClick={restQuestions} style={{ margin: 20 }}>
+        Rest
+      </Button>
     </div>
   );
 }
---
+++ src/questionsStore.js
@@ -0,0 +1,164 @@
+export const football = [
+    {   id:0,
+        text:"who's the best football player in the history ?",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"easy",
+        marks:'10'
+        // marks:easyMark/queasionsTypes.easyQuestionsCount
+    },
+    {   id:1,
+        text:"Which player scored the fastest hat-trick in the Premier League ?",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"easy",
+        marks:'10'
+        // marks:mediumMark/queasionsTypes.mediumQuestionsCount
+    },
+    {   id:2,
+        text:"Which player, with 653 games, has made the most Premier League appearances ?",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"medium",
+        marks:'10'
+        // marks:mediumMark/queasionsTypes.mediumQuestionsCount
+    },
+    {   id:3,
+        text:"who is the best Mohamed Salah, C.Ronaldo or Zlatan Ibrahimović ?",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"medium",
+        marks:'10'
+        // marks:mediumMark/queasionsTypes.mediumQuestionsCount
+    }, 
+    {   id:4,
+        text:"When was the inaugural Premier League season?",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"medium",
+        marks:'10'
+        // marks:mediumMark/queasionsTypes.mediumQuestionsCount
+    }, 
+    {   id:5,
+        text:"Which team won the first Premier League title ?",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"medium",
+        marks:'10'
+        // marks:mediumMark/queasionsTypes.mediumQuestionsCount
+    }, 
+    {   id:6,
+        text:"With 202 clean sheets, which goalkeeper has the best record in the Premier League?",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"medium",
+        marks:'10'
+        // marks:hardMark/queasionsTypes.hardQuestionsCount
+    }, 
+    {   id:7,
+        text:"Which three players shared the Premier League Golden Boot in 2018-19 ?",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"hard",
+        marks:'10'
+        // marks:hardMark/queasionsTypes.hardQuestionsCount
+    }, 
+    {   id:8,
+        text:"The fastest goal scored in Premier League history came in 7.69 seconds. Who scored it?",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"hard",
+        marks:'10'
+        // marks:hardMark/queasionsTypes.hardQuestionsCount
+    }, 
+    {   id:9,
+        text:"Which club has won the most Champions League titles?",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"hard",
+        marks:'10'
+        // marks:easyMark/queasionsTypes.easyQuestionsCount
+    }, 
+]
+export const Geography  = [
+    {   id:0,
+        text:"	What is the name of the part of the map that shows directions?",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"easy",
+        marks:'10'
+        // marks:easyMark/queasionsTypes.easyQuestionsCount
+    },
+    {   id:1,
+        text:"What part of the map shows how distance on the map compares to distance on the ground?",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"easy",
+        marks:'10'
+        // marks:mediumMark/queasionsTypes.mediumQuestionsCount
+    },
+    {   id:2,
+        text:"	What part of the map explains what its symbols mean?",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"medium",
+        marks:'10'
+        // marks:mediumMark/queasionsTypes.mediumQuestionsCount
+    },
+    {   id:3,
+        text:"What is the name of the lines that run horizontally across the map?",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"medium",
+        marks:'10'
+        // marks:mediumMark/queasionsTypes.mediumQuestionsCount
+    }, 
+    {   id:4,
+        text:"What is the name of the lines that run vertically across the map?",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"medium",
+        marks:'10'
+        // marks:mediumMark/queasionsTypes.mediumQuestionsCount
+    }, 
+    {   id:5,
+        text:"The____serves as an important transportation link between Iran and other Asian countries",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"medium",
+        marks:'10'
+        // marks:mediumMark/queasionsTypes.mediumQuestionsCount
+    }, 
+    {   id:6,
+        text:"Longitude runs____and measures____",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"medium",
+        marks:'10'
+        // marks:hardMark/queasionsTypes.hardQuestionsCount
+    }, 
+    {   id:7,
+        text:"Latitude runs____and measures____",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"hard",
+        marks:'10'
+        // marks:hardMark/queasionsTypes.hardQuestionsCount
+    }, 
+    {   id:8,
+        text:"Longitude, on a map, measures from 0 degrees to___",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"hard",
+        marks:'10'
+        // marks:hardMark/queasionsTypes.hardQuestionsCount
+    }, 
+    {   id:9,
+        text:"Latitude, on a map, measures from 0 degrees to___",
+        subject:"public_info",
+        topic:"sports_info",
+        difficulty:"hard",
+        marks:'10'
+        // marks:easyMark/queasionsTypes.easyQuestionsCount
+    }, 
+]
